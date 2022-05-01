2296c31270881c10be8e99aa8bb6595c89167ad3
Anon-K <50687833+AnonkFLY@users.noreply.github.com>
2022-05-02 00:34:35 +0800

    找不同

---
+++ 389.找不同.cs
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=389 lang=csharp
+ *
+ * [389] 找不同
+ */
+
+// @lc code=start
+public class Solution
+{
+    //tired -5.1
+    public char FindTheDifference(string s, string t)
+    {
+        int sum = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            sum += s[i];
+            sum -= t[i];
+        }
+        sum -= t[t.Length - 1];
+        return (char)-sum;
+    }
+}
+// @lc code=end
+
